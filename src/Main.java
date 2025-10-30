430ae4211e1c6b600cba4e18b24904e35d54767d
ElvisOsmanov <elviso10@nycstudents.net>
2025-10-30 15:02:13 -0400

    finished

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
         public class Main {
     public static void main(String[] args) {
         APCalendar.printLeapYear(2024);
-        APCalendar.printLeapYear(2022);
+        APCalendar.printLeapYear(2021);
 
         System.out.println(APCalendar.numberOfLeapYears(1492, 1776));
 
